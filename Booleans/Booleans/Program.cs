582fce3991fdcba1a04ce85a75400ccee60f3d26
Daniel-Hercules <73712933+Daniel-Hercules@users.noreply.github.com>
2020-11-02 14:34:42 +0000

    Add files via upload

---
+++ Booleans/Booleans/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Booleans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaration of Boolean variables 
+            Boolean isCSharpFun = true;
+            Boolean isTheWorldGoingToEnd = false;
+
+            // Typecasting the value of the variables
+            Console.WriteLine(isCSharpFun);
+            Console.WriteLine(isTheWorldGoingToEnd);
+
+            // Printing out result of boolean arguement
+            
+            int x = 10;
+            int y = 10;
+
+            // Prints out if x is bigger than y
+            Console.WriteLine(x > y);
+
+            // Prints out if x and y are equal
+            Console.WriteLine(x == y);
+
+
+        }
+    }
+}
